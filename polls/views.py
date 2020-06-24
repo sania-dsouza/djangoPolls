88d96e25a47d77befba36b745173ec5237f6c18e
Sania <sania.d@myplanet.com>
2020-06-24 13:36:07 -0400

    Changes after Tut 5

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from django.http import Http404
 #from django.template import loader
 from .models import Choice, Question
@@ -12,11 +13,18 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('pub_date')[0:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[0:5]
+
 
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -26,6 +34,7 @@ class ResultsView(generic.DetailView):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/detail.html', {'question': question})
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
@@ -33,7 +42,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question':question,
+            'question': question,
             'error_message': "You didn't select a choice",
         })
 
